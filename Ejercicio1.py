3b447a06a62edf848310550997ccbd6b5a2c5398
julian0818 <60899000+julian0818@users.noreply.github.com>
2020-02-10 15:17:17 -0500

    Add files via upload

---
+++ Ejercicio1.py
@@ -0,0 +1,5 @@
+edad = int(input("Ingrese su edad"))
+print("Su edad actual: " + str(edad))
+edadFuturo = int(input("Cuantos años desea agregar?"))
+edadTotal = edad+edadFuturo
+print("Dentro de " + str(edadFuturo) + "usted tendrá " + str(edadTotal))
